4a1aa99ee5a8e72caa0ee6637fd4ad9a29a6b9a5
Marcio Cavallieri <mvccavallieri@gmail.com>
2020-07-19 18:44:07 -0300

    Criação da classe Departamento

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Departamento departamento = new Departamento(1, "Livros");
+	}
+
+}
