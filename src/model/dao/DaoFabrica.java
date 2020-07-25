8b01f1cae11ee26d86855a1401e3792d34d18771
Marcio Cavallieri <mvccavallieri@gmail.com>
2020-07-25 11:41:09 -0300

    Implementação da função ObterPorId

--- src/model/dao/DaoFabrica.java
+++ src/model/dao/DaoFabrica.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.DepartamentoDaoJDBC;
 import model.dao.impl.VendedorDaoJDBC;
 
 public class DaoFabrica {
 	public static VendedorDao criarVendedorDao() {
-		return new VendedorDaoJDBC();
+		return new VendedorDaoJDBC(DB.getConnection());
 	}
 	
 	public static DepartamentoDao criarDepartamentoDao() {
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -1,34 +1,85 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DBException;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
-public class VendedorDaoJDBC implements VendedorDao{
+public class VendedorDaoJDBC implements VendedorDao {
+	private Connection conexao;
+	
+	public VendedorDaoJDBC(Connection conexao) {
+		this.conexao = conexao;
+	}
 
 	@Override
 	public void Inserir(Vendedor v) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void Atualizar(Vendedor v) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void DeletarPeloId(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Vendedor ObterPeloId(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement preparedStatement = null; 
+		ResultSet resultSet = null;
+				
+		try {
+			preparedStatement = conexao.prepareStatement(
+					"SELECT " + 
+					"  V.*, " +
+					"  D.nome AS departamento_nome" +
+					"FROM " +
+					"  vendedor AS V" +
+					"INNER JOIN Departamento AS D" +
+					"  ON (V.departamento_id = D.id)" +
+					"WHERE" +
+					"  (V.id = ?)");
+			
+			preparedStatement.setInt(0, id);
+			resultSet = preparedStatement.executeQuery();
+			
+			if (resultSet.next()) {
+				Departamento dep = new Departamento();
+				dep.setId(resultSet.getInt("departamento_id"));
+				dep.setNome(resultSet.getString("departamento_nome"));
+				
+				Vendedor vend = new Vendedor();
+				vend.setId(resultSet.getInt("id"));
+				vend.setNome(resultSet.getString("nome"));
+				vend.setEmail(resultSet.getString("email"));
+				vend.setDataNascimento(resultSet.getDate("data_nascimento"));
+				vend.setSalarioBase(resultSet.getDouble("salario_base"));
+				vend.setDepartamento(dep);
+				
+				return vend;
+			}
+			
+			return null;
+		} catch (SQLException e){
+			throw new DBException(e.getMessage());
+		}finally {
+			DB.fecharPreparedStatement(preparedStatement);
+			DB.fecharResultSet(resultSet);
+		}
 	}
 
 	@Override
