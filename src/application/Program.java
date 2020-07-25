d5811e39100c097f5d19b99f3dccc9bc7cd8783b
Marcio Cavallieri <mvccavallieri@gmail.com>
2020-07-25 10:34:17 -0300

    Criação da classe Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Departamento departamento = new Departamento(1, "Livros"); //
+		Departamento departamento = new Departamento(1, "Livros"); 
+		Vendedor vendedor = new Vendedor(21, "Marcio", "Marcio@Marcio.br", new Date(), 3000.0, departamento);
+		
+		System.out.println(vendedor);
 	}
 
 }
